dff6335d8aac499597f24c7661828fabecd7ecee
frank1738 <franklineosoro08@gmail.com>
2022-09-16 14:30:32 +0300

    update category attribute

--- db/migrate/20220916080141_create_hotels.rb
+++ db/migrate/20220916080141_create_hotels.rb
@@ -4,12 +4,12 @@ class CreateHotels < ActiveRecord::Migration[7.0]
       t.string :name
       t.decimal :price
       t.text :description
-      t.string :category
+      t.integer :category
       t.string :country
       t.string :city
       t.text :address
       t.string :image_url
-      t.references :category, index: true, foreign_key: { to_table: :categories }
+      t.references :category_id, index: true, foreign_key: { to_table: :categories }
 
       t.timestamps
     end
