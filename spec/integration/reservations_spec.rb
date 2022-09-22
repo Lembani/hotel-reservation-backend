27a0e1b8ead97fac31450a005fcd1560837ab4ec
ekenecf <ekens2u@gmail.com>
2022-09-22 21:06:45 +0100

    Create reservation for integration spec

--- spec/integration/reservations_spec.rb
+++ spec/integration/reservations_spec.rb
@@ -1,1 +1,7 @@
 require 'rails_helper'
+
+RSpec.describe 'GET /api/v1/hotels/:id/reservations', type: :feature do
+  let(:reservation) do
+    Reservation.create(reason: 'I cant go, its late', duration: '2days', start_day: '2020-09-21', end_day: '2022-09-22')
+  end
+end
