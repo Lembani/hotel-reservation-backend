03c85d11531fc68ef20fa44de62ea8510f5b080f
ekenecf <ekens2u@gmail.com>
2022-09-22 21:20:30 +0100

    Create hotel for integration spec

--- spec/integration/reservations_spec.rb
+++ spec/integration/reservations_spec.rb
@@ -5,5 +5,10 @@ RSpec.describe 'GET /api/v1/hotels/:id/reservations', type: :feature do
     Reservation.create(reason: 'I cant go, its late', duration: '2days', start_day: '2020-09-21', end_day: '2022-09-22')
   end
   let(:category) { Category.create(name: 'Konja', description: 'Amazing world', rating: 5) }
+  let(:hotel) do
+    Hotel.create(name: 'Jango', price: '1000', description: 'Best hotel', country: 'Nigeria', category:, city: 'lagos',
+                 address: 'lagos', image_url: 'image.jpg')
+  end
+
  
 end
