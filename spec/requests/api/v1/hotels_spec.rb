78e45e87db05ec78c1b260e7c52631819e415a09
frank1738 <franklineosoro08@gmail.com>
2022-09-24 01:01:02 +0300

    add hotels tests

---
+++ spec/requests/api/v1/hotels_spec.rb
@@ -0,0 +1,133 @@
+require 'swagger_helper'
+
+RSpec.describe 'api/v1/hotels', type: :request do
+  path '/api/v1/hotels' do
+    get('list hotels') do
+      response(200, 'successful') do
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    post('create hotel') do
+      tags 'Hotel'
+      consumes 'application/json'
+      parameter name: :hotel, in: :body, schema: {
+        type: :object,
+        properties: {
+          name: { type: :string },
+          description: { type: :string },
+          price: { type: :integer },
+          country: { type: :string },
+          city: { type: :string },
+          address: { type: :string },
+          image_url: { type: :string }
+        },
+        required: %w[name description price country city address image_url category_id]
+      }
+      response '201', 'Hotel created' do
+        let(:category) { Category.create(name: 'Konja', description: 'Amazing world', rating: 5) }
+        let(:hotel) do
+          Hotel.create(name: 'Jango', price: '1000', description: 'Best hotel', country: 'Nigeria',
+                       category:, city: 'lagos', address: 'lagos', image_url: 'image.jpg')
+        end
+        run_test!
+      end
+
+      response '422', 'invalid request' do
+        let(:hotel) { { name: 'foo' } }
+        run_test!
+      end
+    end
+  end
+  path '/api/v1/hotels/{id}' do
+    # You'll want to customize the parameter types...
+    parameter name: 'id', in: :path, type: :string, description: 'id'
+
+    get('show Hotel') do
+      response(200, 'successful') do
+        let(:id) { Category.create(name: 'Platinum', description: 'Top rated!', rating: 5).id }
+        let(:id) do
+          Hotel.create(name: 'Jango', price: '1000', description: 'Best hotel', country: 'Nigeria',
+                       category_id: 1, city: 'lagos', address: 'lagos', image_url: 'image.jpg').id
+        end
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+
+      response '200', 'Hotel not found' do
+        let(:id) { '' }
+        run_test!
+      end
+    end
+
+    patch('update category') do
+      let(:id) { Category.create(name: 'Platinum', description: 'Top rated!', rating: 5).id }
+      let(:id) do
+        Hotel.create(name: 'Jango', price: '1000', description: 'Best hotel', country: 'Nigeria',
+                     category_id: 1, city: 'lagos', address: 'lagos', image_url: 'image.jpg').id
+      end
+      tags 'Hotel'
+      consumes 'application/json'
+      parameter name: :hotel, in: :body, schema: {
+        type: :object,
+        properties: {
+          name: { type: :string },
+          description: { type: :string },
+          price: { type: :integer },
+          country: { type: :string },
+          city: { type: :string },
+          address: { type: :string },
+          image_url: { type: :string }
+        },
+        required: %w[name description price country city address image_url category_id]
+      }
+    end
+
+    put('update hotel') do
+      let(:id) { Category.create(name: 'Platinum', description: 'Top rated!', rating: 5).id }
+      let(:id) do
+        Hotel.create(name: 'Jango', price: '1000', description: 'Best hotel', country: 'Nigeria',
+                     category_id: 1, city: 'lagos', address: 'lagos', image_url: 'image.jpg').id
+      end
+      tags 'Hotel'
+      consumes 'application/json'
+      parameter name: :hotel, in: :body, schema: {
+        type: :object,
+        properties: {
+          name: { type: :string },
+          description: { type: :string },
+          price: { type: :integer },
+          country: { type: :string },
+          city: { type: :string },
+          address: { type: :string },
+          image_url: { type: :string }
+        },
+        required: %w[name description price country city address image_url category_id]
+      }
+    end
+
+    delete('delete category') do
+      response(204, 'successful') do
+        let(:id) { Category.create(name: 'Platinum', description: 'Top rated!', rating: 5).id }
+        let(:id) do
+          Hotel.create(name: 'Jango', price: '1000', description: 'Best hotel', country: 'Nigeria',
+                       category_id: 1, city: 'lagos', address: 'lagos', image_url: 'image.jpg').id
+        end
+      end
+    end
+  end
+end
