8d4f9bb6352799c3463512023c164893fd4c5900
ekenecf <ekens2u@gmail.com>
2022-09-24 15:45:15 +0100

    Remove unwanted codes from codebase

--- spec/requests/api/v1/reservations_spec.rb
+++ spec/requests/api/v1/reservations_spec.rb
@@ -62,7 +62,6 @@ RSpec.describe '/api/v1/hotels/{hotel_id}/reservations', type: :request do
   it do
 
     path "/api/v1/hotels/#{hotel_id}/reservations/#{res_id}" do
-      # You'll want to customize the parameter types...
       parameter name: 'id', in: :path, type: :string, description: 'id'
       get('show Hotel') do
         tags 'Show Hotel'
