2e7fc4fd6073acf4b55bc62aaa23c7a242238732
ekenecf <ekens2u@gmail.com>
2022-09-26 12:35:50 +0100

    Update tests for reservations

--- spec/requests/api/v1/reservations_spec.rb
+++ spec/requests/api/v1/reservations_spec.rb
@@ -1,135 +1,209 @@
 require 'swagger_helper'
 
-RSpec.describe '/api/v1/hotels/{hotel_id}/reservations', type: :request do
-  let(:user) { User.create!(name: 'example', email: 'example.com', password: '123456') }
-  let(:category) { Category.create!(name: 'Konja', description: 'Amazing world', rating: 5) }
-  let(:hotel) do
-    Hotel.create!(name: 'Jango', price: '1000', description: 'Best hotel', country: 'Nigeria', category:, city: 'lagos',
-                  address: 'lagos', image_url: 'image.jpg')
-  end
-  let(:hotel_id) { hotel.id }
-  let(:reservation) do
-    Reservation.create!(user:, hotel:, reason: 'I cant go, its late', duration: '2days',
-                        start_day: '2020-09-21', end_day: '2022-09-22')
-  end
-  let(:res_id) { reservation.id }
+RSpec.describe 'api/v1/reservations', type: :request do
+  path '/api/v1/hotels/{hotel_id}/reservations' do
+    # You'll want to customize the parameter types...
+    parameter name: 'hotel_id', in: :path, type: :integer, description: 'hotel_id'
 
-  it do
-    path "/api/v1/hotels/#{hotel_id}/reservations" do
-      get('list reservations') do
-        tags 'Reservations List'
-        response(200, 'successful') do
-          after do |example|
-            example.metadata[:response][:content] = {
-              'application/json' => {
-                example: JSON.parse(response.body, symbolize_names: true)
-              }
+    get('list reservations') do
+      response(200, 'successful') do
+        let(:id) { Category.create(name: 'Platinum', description: 'Top rated!', rating: 5).id }
+
+        let(:hotel_id) do
+          Hotel.create(name: 'Jango', price: '1000', description: 'Best hotel', country: 'Nigeria',
+                       category_id: id, city: 'lagos', address: 'lagos', image_url: 'image.jpg').id
+        end
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
             }
-          end
-          run_test!
+          }
         end
+        run_test!
+      end
+    end
+
+    post('create reservation') do
+      tags 'Create Hotel'
+      consumes 'application/json'
+      parameter name: :reservation, in: :body, schema: {
+        type: :object,
+        properties: {
+          reason: { type: :text },
+          duration: { type: :string },
+          start_day: { type: :datetime },
+          end_day: { type: :datetime },
+          user_id: { type: :integer },
+          hotel_id: { type: :integer }
+        },
+        required: %w[reason duration start_day end_day user_id hotel_id]
+      }
+      response(201, 'successful') do
+        let(:user_id) do
+          User.create!(name: 'example', email: 'example@gmail.com', password: '123456',
+                       password_confirmation: '123456').id
+        end
+        let(:category_id) { Category.create(name: 'Platinum', description: 'Top rated!', rating: 5).id }
+        let(:hotel) do
+          Hotel.create(name: 'Jango', price: '1000', description: 'Best hotel', country: 'Nigeria',
+                       category_id:, city: 'lagos', address: 'lagos', image_url: 'image.jpg')
+        end
+        let(:hotel_id) { hotel.id }
+        let(:reservation) do
+          Reservation.create!(reason: 'I am tired', duration: '2 days', start_day: '2020-10-10', end_day: '2022-10-10',
+                              user_id:, hotel_id:)
+        end
+        run_test!
       end
     end
   end
 
-  it do
-    path "/api/v1/hotels/#{hotel_id}/reservations" do
-      post('create reservation') do
-        tags 'Create Reservation'
-        consumes 'application/json'
-        parameter name: :reservation, in: :body, schema: {
-          type: :object,
-          properties: {
-            reason: { type: :text },
-            duration: { type: :string },
-            start_day: { type: :datetime },
-            end_day: { type: :datetime }
-          },
-          required: %w[reason duration start_day end_day]
-        }
-        response '201', 'reservation created' do
-          reservation
-          run_test!
+  # rubocop:disable Metrics/BlockLength
+  path '/api/v1/hotels/{hotel_id}/reservations/{id}' do
+    # You'll want to customize the parameter types...
+    parameter name: 'hotel_id', in: :path, type: :string, description: 'hotel_id'
+    parameter name: 'id', in: :path, type: :string, description: 'id'
+
+    get('show reservation') do
+      response(200, 'successful') do
+        let(:user_id) do
+          User.create!(name: 'example', email: 'example@gmail.com', password: '123456',
+                       password_confirmation: '123456').id
         end
-        response '422', 'invalid request' do
-          let(:reservation) { { reason: 'I am tired' } }
-          run_test!
+        let(:category_id) { Category.create(name: 'Platinum', description: 'Top rated!', rating: 5).id }
+        let(:hotel) do
+          Hotel.create(name: 'Jango', price: '1000', description: 'Best hotel', country: 'Nigeria',
+                       category_id:, city: 'lagos', address: 'lagos', image_url: 'image.jpg')
         end
-      end
-    end
-  end
 
-  it do
+        let(:hotel_id) { hotel.id }
+        let(:reservation) do
+          Reservation.create!(reason: 'I am tired', duration: '2 days', start_day: '2020-10-10', end_day: '2022-10-10',
+                              user_id:, hotel_id:)
+        end
 
-    path "/api/v1/hotels/#{hotel_id}/reservations/#{res_id}" do
-      parameter name: 'id', in: :path, type: :string, description: 'id'
-      get('show Hotel') do
-        tags 'Show Hotel'
-        response(200, 'successful') do
-          let(:reservation) { reservation.id }
+        let(:id) { reservation.id }
 
-          after do |example|
-            example.metadata[:response][:content] = {
-              'application/json' => {
-                example: JSON.parse(response.body, symbolize_names: true)
-              }
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
             }
-          end
-          run_test!
+          }
         end
-        response '200', 'Hotel not found' do
-          let(:id) { '' }
-          run_test!
-        end
+        run_test!
       end
     end
-  end
 
-  it do
-    path "/api/v1/hotels/#{hotel_id}/reservations/#{res_id}" do
-      patch('Update reservation') do
-        tags 'Update Reservation'
-        consumes 'application/json'
-        parameter name: :reservation, in: :body, schema: {
-          type: :object,
-          properties: {
-            reason: { type: :text },
-            duration: { type: :string },
-            start_day: { type: :datetime },
-            end_day: { type: :datetime }
-          },
-          required: %w[reason duration start_day end_day]
-        }
-        response '200', 'reservation updated' do
-          reservation
-          let(:id) { reservation.id }
-          run_test!
+    patch('update reservation') do
+      tags 'Update Hotel'
+      consumes 'application/json'
+      parameter name: :reservation, in: :body, schema: {
+        type: :object,
+        properties: {
+          reason: { type: :text },
+          duration: { type: :string },
+          start_day: { type: :datetime },
+          end_day: { type: :datetime },
+          user_id: { type: :integer },
+          hotel_id: { type: :integer }
+        },
+        required: %w[reason duration start_day end_day user_id hotel_id]
+      }
+      response(200, 'successful') do
+        let(:user_id) do
+          User.create!(name: 'example', email: 'example@gmail.com', password: '123456',
+                       password_confirmation: '123456').id
         end
+        let(:category_id) { Category.create(name: 'Platinum', description: 'Top rated!', rating: 5).id }
+        let(:hotel) do
+          Hotel.create(name: 'Jango', price: '1000', description: 'Best hotel', country: 'Nigeria',
+                       category_id:, city: 'lagos', address: 'lagos', image_url: 'image.jpg')
+        end
+        let(:hotel_id) { hotel.id }
+        let(:reservation) do
+          Reservation.create!(reason: 'I am tired', duration: '2 days', start_day: '2020-10-10', end_day: '2022-10-10',
+                              user_id:, hotel_id:)
+        end
+        let(:id) { reservation.id }
+
+        run_test!
       end
     end
-  end
 
-  it do
-    path "/api/v1/hotels/#{hotel_id}/reservations/#{res_id}" do
-      delete('delete reservation') do
-        tags 'delete Reservation'
-        consumes 'application/json'
-        parameter name: :reservation, in: :body, schema: {
-          type: :object,
-          properties: {
-            reason: { type: :text },
-            duration: { type: :string },
-            start_day: { type: :datetime },
-            end_day: { type: :datetime }
-          },
-          required: %w[reason duration start_day end_day]
-        }
-        response '200', 'reservation deleted' do
-          reservation
-          let(:id) { reservation.id }
-          run_test!
+    put('update reservation') do
+      tags 'Update Hotel'
+      consumes 'application/json'
+      parameter name: :reservation, in: :body, schema: {
+        type: :object,
+        properties: {
+          reason: { type: :text },
+          duration: { type: :string },
+          start_day: { type: :datetime },
+          end_day: { type: :datetime },
+          user_id: { type: :integer },
+          hotel_id: { type: :integer }
+        },
+        required: %w[reason duration start_day end_day user_id hotel_id]
+      }
+      response(200, 'successful') do
+        let(:user_id) do
+          User.create!(name: 'example', email: 'example@gmail.com', password: '123456',
+                       password_confirmation: '123456').id
         end
+        let(:category_id) { Category.create(name: 'Platinum', description: 'Top rated!', rating: 5).id }
+        let(:hotel) do
+          Hotel.create(name: 'Jango', price: '1000', description: 'Best hotel', country: 'Nigeria',
+                       category_id:, city: 'lagos', address: 'lagos', image_url: 'image.jpg')
+        end
+        let(:hotel_id) { hotel.id }
+        let(:reservation) do
+          Reservation.create!(reason: 'I am tired', duration: '2 days', start_day: '2020-10-10', end_day: '2022-10-10',
+                              user_id:, hotel_id:)
+        end
+        let(:id) { reservation.id }
+
+        run_test!
+      end
+    end
+
+    delete('update reservation') do
+      tags 'Update Hotel'
+      consumes 'application/json'
+      parameter name: :reservation, in: :body, schema: {
+        type: :object,
+        properties: {
+          reason: { type: :text },
+          duration: { type: :string },
+          start_day: { type: :datetime },
+          end_day: { type: :datetime },
+          user_id: { type: :integer },
+          hotel_id: { type: :integer }
+        },
+        required: %w[reason duration start_day end_day user_id hotel_id]
+      }
+      response(200, 'successful') do
+        let(:user_id) do
+          User.create!(name: 'example', email: 'example@gmail.com', password: '123456',
+                       password_confirmation: '123456').id
+        end
+        let(:category_id) { Category.create(name: 'Platinum', description: 'Top rated!', rating: 5).id }
+        let(:hotel) do
+          Hotel.create(name: 'Jango', price: '1000', description: 'Best hotel', country: 'Nigeria',
+                       category_id:, city: 'lagos', address: 'lagos', image_url: 'image.jpg')
+        end
+        let(:hotel_id) { hotel.id }
+        let(:reservation) do
+          Reservation.create!(reason: 'I am tired', duration: '2 days', start_day: '2020-10-10', end_day: '2022-10-10',
+                              user_id:, hotel_id:)
+        end
+        let(:id) { reservation.id }
+
+        run_test!
       end
     end
   end
 end
+# rubocop:enable Metrics/BlockLength
