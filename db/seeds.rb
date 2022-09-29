56825206fce282164969e06ebf7bb9c84a2565dd
Lembani <Lembanisakala@gmail.com>
2022-09-29 17:56:28 +0200

    Add User seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,23 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
+
+users = User.create!([
+    {
+        name: "Tom",
+        email: "tom@gmail.com",
+        password: "123456",
+        password_confirmation: "123456",
+        admin: true,
+    },
+    {
+        name: "Frank",
+        email: "frank@gmail.com",
+        password: "123456",
+        password_confirmation: "123456",
+    }
+])
+
 categories = Category.create!([{ name: 'The Unrated', description: 'Hotels under this category have not been rated yet' }, { name: 'Bronze Ultra', description: 'The lowest rated category', rating: 1 }, { name: 'Silver Ultra', description: 'The second lowest rated category', rating: 2 }, { name: 'Gold Ultra', description: 'The third highest rated category', rating: 3 }, { name: 'Diamond Ultra', description: 'The second highest category', rating: 4 }, { name: 'Platinum Ultra', description: 'The highest rated category', rating: 5 }])
 
 hotels = Hotel.create!([
