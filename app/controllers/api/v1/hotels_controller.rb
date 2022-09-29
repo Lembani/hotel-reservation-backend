3b4f976bf440177c15da0e07494b852412dec74d
ekenecf <ekens2u@gmail.com>
2022-09-29 16:28:19 +0100

    Fix rubocop offence

--- app/controllers/api/v1/hotels_controller.rb
+++ app/controllers/api/v1/hotels_controller.rb
@@ -1,22 +1,15 @@
 class Api::V1::HotelsController < ApplicationController
-
-
   # before_action :logged_in, except: %i[index show]
 
   # before_action :user_ability, except: %i[index show]
 
   def index
-
     @hotels = Hotel.all
 
     render json: { status: 'Success', message: 'loaded hotels', hotels: @hotels }, status: :ok
-
   end
 
-
-
   def show
-
     @hotel = Hotel.includes(:reservations).find(params[:id])
 
     @reservations = @hotel.reservations.order(created_at: :desc)
@@ -24,13 +17,9 @@ class Api::V1::HotelsController < ApplicationController
     render json: { status: 'Success', message: 'loaded hotel', hotel: @hotel, reservations: @reservations },
 
            status: :ok
-
   end
 
-
-
   def create
-
     @hotel = Hotel.new(hotel_params)
 
     if @hotel.save
@@ -50,13 +39,9 @@ class Api::V1::HotelsController < ApplicationController
       }, status: :unprocessable_entity
 
     end
-
   end
 
-
-
   def destroy
-
     @hotel = Hotel.find(params[:id])
 
     if @hotel.destroy
@@ -68,13 +53,9 @@ class Api::V1::HotelsController < ApplicationController
       render json: { message: 'Something went wrong' }, status: :unprocessable_entity
 
     end
-
   end
 
-
-
   def update
-
     @hotel = Hotel.find(params[:id])
 
     if @hotel.update(hotel_params)
@@ -86,34 +67,19 @@ class Api::V1::HotelsController < ApplicationController
       render json: { message: 'Something went wrong' }, status: :unprocessable_entity
 
     end
-
   end
-
-
 
   private
 
-
-
   def hotel_params
-
     params.require(:hotel).permit(:name, :price, :description, :country, :city, :address, :image_url, :category_id)
-
   end
 
-
-
   def user_ability
-
     authorize! :manage, @hotel
-
   rescue CanCan::AccessDenied
-
     render json: { errors: 'You are not authorized to perform this action' },
 
            status: :unauthorized
-
   end
-
 end
-
